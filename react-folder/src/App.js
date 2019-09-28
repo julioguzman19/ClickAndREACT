2ae5c3d91e84b34b64071ef1d943c948a78637fc
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-09-28 09:25:27 -0600

    Nav bar above wrapper formatted

--- react-folder/src/App.js
+++ react-folder/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import friends from "./friends.json";
+import Title from "./components/Title";
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
@@ -19,19 +20,22 @@ class App extends Component {
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
-      <Wrapper>
-        {this.state.friends.map(friend => (
-          <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
-          />
-        ))}
-      </Wrapper>
+      <div>
+        <Title>Friends List</Title>
+        <Wrapper>
+          {this.state.friends.map(friend => (
+            <FriendCard
+              removeFriend={this.removeFriend}
+              id={friend.id}
+              key={friend.id}
+              name={friend.name}
+              image={friend.image}
+              occupation={friend.occupation}
+              location={friend.location}
+            />
+          ))}
+        </Wrapper>
+      </div>
     );
   }
 }
--- react-folder/src/components/Title/index.js
+++ react-folder/src/components/Title/index.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./style.css";
 
 function Title(props) {
-  return <h1 className="title">{props.children}</h1>;
+  return <div>
+     <nav class="navbar">
+         <ul>
+             <li>li1</li>
+             <li>li2</li>
+             <li>li3</li>
+         </ul>
+     </nav>
+     <h1 className="title">{props.children}</h1>
+     </div>;
 }
 
 export default Title;
